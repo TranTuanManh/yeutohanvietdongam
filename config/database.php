7277b797621c79f62cbd2b4c1f9db4327fc974c7
trantuanmanh <manhit1706@gmail.com>
2018-04-15 12:11:51 +0700

    update database

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'sql9.freemysqlhosting.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'sql9232737'),
+            'username' => env('DB_USERNAME', 'sql9232737'),
+            'password' => env('DB_PASSWORD', 'J7Rr5Vnx4b'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
